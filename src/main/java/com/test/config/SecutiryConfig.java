11058b879aadd527c6e7e98de0d11af98d35bfbb
殷婷婷 <302899500@qq.com>
2019-04-09 12:57:17 +0800

    9. 登录成功处理

--- src/main/java/com/test/config/SecutiryConfig.java
+++ src/main/java/com/test/config/SecutiryConfig.java
@@ -8,12 +8,16 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 public class SecutiryConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private SecurityProperties securityProperties;
+
+    @Autowired
+    private AuthenticationSuccessHandler authenticationSuccessHandler;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
@@ -24,6 +28,7 @@ public class SecutiryConfig extends WebSecurityConfigurerAdapter {
         http.formLogin()
                 .loginPage("/authentication/require")
                 .loginProcessingUrl("/authentication/form")
+                .successHandler(authenticationSuccessHandler)
                 .and()
                 .authorizeRequests()
                 .antMatchers("/authentication/require","/authentication/"+securityProperties.getBrowser().getLoginPage()).permitAll()
