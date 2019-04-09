3099034449c73083181cecb967dbd5b82018c8df
殷婷婷 <302899500@qq.com>
2019-04-09 14:17:14 +0800

    12. 获取当前用户信息

--- src/main/java/com/test/controller/BrowserSecurityController.java
+++ src/main/java/com/test/controller/BrowserSecurityController.java
@@ -3,6 +3,8 @@ package com.test.controller;
 import com.test.properties.SecurityProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.DefaultRedirectStrategy;
 import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
@@ -27,8 +29,7 @@ public class BrowserSecurityController {
 
     @RequestMapping("authentication/require")
     @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
-    public String requireAuthentication(HttpServletRequest request, HttpServletResponse response)throws IOException {
-        SavedRequest savedRequest = requestCache.getRequest(request,response);
+    public String requireAuthentication(HttpServletRequest request, HttpServletResponse response)throws IOException {SavedRequest savedRequest = requestCache.getRequest(request,response);
         if (savedRequest != null){
             String targetUrl = savedRequest.getRedirectUrl();
             System.out.println("引发跳转的请求：" + targetUrl);
@@ -38,5 +39,15 @@ public class BrowserSecurityController {
             }
         }
         return "访问的服务需要身份认证，请引导用户到登录页";
+    }
+
+    @RequestMapping("me1")
+    public Authentication me1(){
+        return SecurityContextHolder.getContext().getAuthentication();
+    }
+
+    @RequestMapping("me2")
+    public Authentication me2(Authentication authentication){
+        return authentication;
     }
 }
